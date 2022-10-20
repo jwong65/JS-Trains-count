c519d9653073b360f17766e8c008057d272002df
unknown <jason.wong6507@gmail.com>
2022-10-20 19:33:21 -0400

    Testing the while loop given

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
 let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
 let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
+
+console.log(newInventoryCarModels);
+let x=0;
+let typeS1Count =0;
+
+while(x<newInventoryCarModels.length){
+    console.log(newInventoryCarModels[x]);
+    if(newInventoryCarModels[x]=='S1'){
+        typeS1Count+=1;
+    }
+    x++;
+}
+console.log("The number of S1 is:", typeS1Count)
